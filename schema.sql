85d98079204d70721b7d594a89b7fa1978233252
Ryuichi Matsuoka <76464810+rmatsuoka@users.noreply.github.com>
2021-12-28 07:14:04 +0000

    define room and room_user table

--- schema.sql
+++ schema.sql
@@ -7,3 +7,22 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`)
 );
+
+DROP TABLE IF EXISTS `room`;
+CREATE TABLE `room` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `live_id` int NOT NULL,
+  `owner` bigint NOT NULL,
+  `status` int NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `room_user`;
+CREATE TABLE `room_user` (
+  `room_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  `score` int,
+  `select_difficulty` int NOT NULL,
+  `judge_count_list` varchar(255),
+  PRIMARY KEY (`room_id`, `user_id`)
+);
