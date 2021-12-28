116b0e3381f04f61660d9dd3dd69fadae58dc84c
Ryuichi Matsuoka <76464810+rmatsuoka@users.noreply.github.com>
2021-12-28 02:49:20 +0000

    implement _get_user_by_token and update_user

--- app/model.py
+++ app/model.py
@@ -43,7 +43,15 @@ def create_user(name: str, leader_card_id: int) -> str:
 
 def _get_user_by_token(conn, token: str) -> Optional[SafeUser]:
     # TODO: 実装
-    pass
+    result = conn.execute(
+        text("SELECT `id`, `name`, `leader_card_id` FROM `user` WHERE `token`=:token"),
+        dict(token=token),
+    )
+    try:
+        row = result.one()
+    except NoResultFound:
+        return None
+    return SafeUser.from_orm(row)
 
 
 def get_user_by_token(token: str) -> Optional[SafeUser]:
@@ -54,5 +62,12 @@ def get_user_by_token(token: str) -> Optional[SafeUser]:
 def update_user(token: str, name: str, leader_card_id: int) -> None:
     # このコードを実装してもらう
     with engine.begin() as conn:
-        # TODO: 実装
-        pass
+        user = _get_user_by_token(conn, token)
+        if user is None:
+            raise InvalidToken
+        conn.execute(
+            text(
+                "UPDATE `user` SET `name` = :name, `leader_card_id` = :leader_card_id WHERE `id` = :id"
+            ),
+            dict(name=name, leader_card_id=leader_card_id, id=user.id),
+        )
